dbe0d4276a108e882bc51c4403a0bc9f279b282f
Manoj <79210178+Manoj0203@users.noreply.github.com>
2026-07-01 16:59:04 +0530

    memo otherdata repo

--- services/firebaseAuth.js
+++ services/firebaseAuth.js
@@ -8,12 +8,12 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDzywwaAKQUI3IwqyVK03-lNk2RFKSKnQM",
-  authDomain: "connect-9cd26.firebaseapp.com",
-  projectId: "connect-9cd26",
-  storageBucket: "connect-9cd26.firebasestorage.app",
-  messagingSenderId: "264923450484",
-  appId: "1:264923450484:web:e9767812d97fbe81fd5521"
+  apiKey: "AKnQM",
+  authDomain: "cm",
+  projectId: "6",
+  storageBucket: "cp",
+  messagingSenderId: "24",
+  appId: "1:21"
 };
 
 let auth;
